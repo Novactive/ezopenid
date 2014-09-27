9016286696c70f17114052ce59a1f7b9a733034b
Guillaume Maïssa <guillaume@maissa.fr>
2014-09-28 01:47:10 +0200

    Fix an error in variable name for attributes mapping

--- settings/ezopenid.ini.append.php
+++ settings/ezopenid.ini.append.php
@@ -6,9 +6,9 @@ Debug=disabled
 ServerUrl=
 LogoutUri=
 # Mapping between eZ publish attributes and CAS ones for user information
-UserAttributes[]
-UserAttributes[first_name]=namePerson_first
-UserAttributes[last_name]=namePerson_last
+AttributesMapping[]
+AttributesMapping[first_name]=namePerson_first
+AttributesMapping[last_name]=namePerson_last
 # Group IDs where the user should be added at creation
 GroupAssignments[]
 GroupAssignments[]=12
